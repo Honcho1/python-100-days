96f1c08d4ea081998669288c54a06eb2f9fbc899
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-02-19 09:02:26 +0100

    Add Day 1 project: Band Name Generator

---
+++ Day001/project.py
@@ -0,0 +1,5 @@
+name = (input("What is your name? \n"))
+print("Welcome to the Band Name Generator!", name)
+city = (input("What is the name of the city you grew up in? \n"))
+pet = (input("What is the name of your pet? \n"))
+print("Your band name could be:", city, pet)
