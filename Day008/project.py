11591286ce7f4a1f818d73fcdf23192837d5e63e
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-03-06 17:00:59 +0100

    Write the function that shifts the message based on the shift amount

--- Day008/project.py
+++ Day008/project.py
@@ -1,1 +1,20 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(message, shift_amount, action):
+    result = []
+    for char in message:
+        if char in alphabet:
+            index = alphabet.index(char)
+            if action == "encode":
+                new_index = (index + shift_amount) % 26
+            elif action == "decode":
+                new_index = (index - shift_amount) % 26
+            else:
+                print("Invalid direction. Choose either 'encode' or 'decode'.")
+                return
+            result.append(alphabet[new_index])
+        else:
+            result.append(char)
+    final_result = "".join(result)
+    print(f"The {action}d text is: {final_result}")
+        
