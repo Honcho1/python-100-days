4d2a0578eeae441152458dd8df45eea14d7b049f
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-06-03 05:37:10 +0100

    Write the function to add bidders

--- Day009/project.py
+++ Day009/project.py
@@ -6,3 +6,8 @@ print(logo)
 def clear_screen():
     """Clears the console screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def add_bidder(bidder_name, bidder_amount, bidders):
+    """Adds a bidder to the bidders dictionary."""
+    bidders[bidder_name] = bidder_amount
+    return bidders
