0ed9f61625db0c359e2d6f94d6c158f69f42b4ca
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-06-03 05:40:25 +0100

    Write the function to find the highest bidder

--- Day009/project.py
+++ Day009/project.py
@@ -11,3 +11,14 @@ def add_bidder(bidder_name, bidder_amount, bidders):
     """Adds a bidder to the bidders dictionary."""
     bidders[bidder_name] = bidder_amount
     return bidders
+
+def find_highest_bidder(bidders):
+    highest_bid = 0
+    winner = ""
+
+    for bidder in bidders:
+        bid_amount = bidders[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    return winner, highest_bid
